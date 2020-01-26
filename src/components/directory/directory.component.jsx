4cc5a3fa2b89969bd2accd2ef9818912609c3329
Giri <hilmangp@gmail.com>
2020-01-26 23:09:28 +0700

    updated our menu-items ti navigate using withRouter

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -49,8 +49,18 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {/* {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+          <MenuItem
+            key={id}
+            title={title}
+            imageUrl={imageUrl}
+            size={size}
+            linkUrl={linkUrl}
+          />
+        ))} dirubah kaya dibawah pake spreading value sama aja equaivalent */}
+
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { WithRouter } from "react-router-dom";
 
 import Directory from "../../components/directory/directory.component";
 
